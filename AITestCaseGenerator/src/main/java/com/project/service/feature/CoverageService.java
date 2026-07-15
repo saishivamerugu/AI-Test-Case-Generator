399704539de961f67ee63328db188b5735c357d1
Sai Shiva <146641544+saishivamerugu@users.noreply.github.com>
2026-07-15 16:34:42 +0530

    Update CoverageService.java

--- AITestCaseGenerator/src/main/java/com/project/service/feature/CoverageService.java
+++ AITestCaseGenerator/src/main/java/com/project/service/feature/CoverageService.java
@@ -12,18 +12,14 @@ public class CoverageService {
     private final PromptBuilderService promptBuilderService;
     private final AIClientService aiClientService;
 
-    public CoverageService(PromptBuilderService promptBuilderService,
-                           AIClientService aiClientService) {
+    public CoverageService(PromptBuilderService promptBuilderService, AIClientService aiClientService) {
         this.promptBuilderService = promptBuilderService;
         this.aiClientService = aiClientService;
     }
 
     public AIResponse generate(String javaCode) {
-
         String prompt = promptBuilderService.buildCoverageSuggestionPrompt(javaCode);
-
         String response = aiClientService.askAI(prompt);
-
         return new AIResponse(response);
     }
 }
