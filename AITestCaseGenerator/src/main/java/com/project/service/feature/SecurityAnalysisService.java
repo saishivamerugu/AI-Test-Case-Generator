fd176e24fcf034557aabff26abea5887ef55fbe0
Sai Shiva <146641544+saishivamerugu@users.noreply.github.com>
2026-07-15 16:36:12 +0530

    Update SecurityAnalysisService.java

--- AITestCaseGenerator/src/main/java/com/project/service/feature/SecurityAnalysisService.java
+++ AITestCaseGenerator/src/main/java/com/project/service/feature/SecurityAnalysisService.java
@@ -12,18 +12,14 @@ public class SecurityAnalysisService {
     private final PromptBuilderService promptBuilderService;
     private final AIClientService aiClientService;
 
-    public SecurityAnalysisService(PromptBuilderService promptBuilderService,
-                                   AIClientService aiClientService) {
+    public SecurityAnalysisService(PromptBuilderService promptBuilderService, AIClientService aiClientService) {
         this.promptBuilderService = promptBuilderService;
         this.aiClientService = aiClientService;
     }
 
     public AIResponse generate(String javaCode) {
-
         String prompt = promptBuilderService.buildSecurityAnalysisPrompt(javaCode);
-
         String response = aiClientService.askAI(prompt);
-
         return new AIResponse(response);
     }
 }
