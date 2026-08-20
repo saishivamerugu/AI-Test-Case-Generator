6345b468cd6c21bf0900715ce8db99dff686dba0
Sai Shiva <146641544+saishivamerugu@users.noreply.github.com>
2026-08-20 22:16:52 +0530

    Refactor AIConfig.java for cleaner formatting

--- AITestCaseGenerator/src/main/java/com/project/config/AIConfig.java
+++ AITestCaseGenerator/src/main/java/com/project/config/AIConfig.java
@@ -5,7 +5,6 @@ import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-
 @Configuration
 public class AIConfig {
  
