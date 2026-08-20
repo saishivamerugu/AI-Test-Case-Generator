7b600540a4ccc731fe1a860ab4e0da6fcfc19e64
Sai Shiva <146641544+saishivamerugu@users.noreply.github.com>
2026-08-20 22:16:41 +0530

    Update AIConfig.java

--- AITestCaseGenerator/src/main/java/com/project/config/AIConfig.java
+++ AITestCaseGenerator/src/main/java/com/project/config/AIConfig.java
@@ -5,6 +5,7 @@ import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+
 @Configuration
 public class AIConfig {
  
